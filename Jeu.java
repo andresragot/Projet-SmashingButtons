b18d87c4d11c43c6a0c2bb4308ca4773679aa19f
andresragot <62946319+andresragot@users.noreply.github.com>
2020-04-07 15:26:25 +0200

    message

--- Jeu.java
+++ Jeu.java
@@ -1,18 +1,69 @@
 public class Jeu {
+
+	private int[][] Level1 = { {0,0,0,0,0,0,0,0,0} , 
+                        {0,2,0,0,0,0,1,1,0} ,
+                        {0,1,0,1,1,0,1,1,0} ,
+                        {0,1,1,1,1,1,0,1,0} ,
+                        {0,0,0,1,1,0,0,1,0} ,
+                        {0,1,1,1,1,1,1,1,3} ,
+                        {0,0,0,0,0,0,0,0,0} };
+                    
+	private int[][] Level2 = { {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} ,
+                        {0,0,0,1,0,1,1,0,1,1,1,1,1,1,0} ,
+                        {0,1,0,1,1,1,1,0,1,0,0,0,1,1,0} ,
+                        {0,1,1,1,1,0,1,0,1,1,1,0,1,1,0} ,
+                        {0,1,0,0,0,0,1,0,1,1,1,0,1,1,0} ,
+                        {0,1,1,1,1,0,1,0,1,1,1,0,1,1,0} ,
+                        {0,0,0,1,1,0,1,1,0,1,0,1,1,1,0} ,
+                        {0,2,1,1,0,1,1,1,0,1,0,1,1,0,0} ,
+                        {0,1,1,0,1,1,1,0,1,1,1,0,1,1,3} ,
+                        {0,1,1,0,1,0,0,0,1,1,1,0,1,1,0} ,
+                        {0,1,1,0,1,1,1,1,1,0,1,1,0,0,0} ,
+                        {0,1,1,1,0,1,0,1,1,0,1,1,1,0,0} ,
+                        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} };
+ 	
+	public int ligne;
+	public Joueur joueur;
+	public int niveauActuel;
+	public int colonne;
+	public int[][] plateau;
+	public int[] hello;
+
+
+	public int comptePoint(){
+		return 2;
+	}
 	
-public Plateau lePlateau;
+	public void addEntite(Entite type){
+	}
 
+	public void bouger(int x, int y){
+	}
+	
+	public boolean colision(){
+		return false;
+	}
+	
 
-public int comptePoint(){
+	public boolean bougerPossible(int x, int y){
+		return true;
+	}
 
-}
+	public void enleverV(Joueur J){
+		int PV = J.getPV()-1;
+		J.setPV(PV);
+	}
 
-public boolean JeuFini(Joueur J){
-	return J.getPV()==0;
-}
+	public int[] positionDepart(int i){
+		return hello;
+	}
 
-public void playSound(String sound){
-}
+	public boolean JeuFini(Joueur J){
+		return J.getPV()==0;
+	}
+
+	public void playSound(String sound){
+	}
 	
 }
 
